8fcbc8c662b55dc6191b12f9904f1489bba47e4e
Capsule Pharmacy <113796169+project088@users.noreply.github.com>
2022-12-27 09:45:35 +0200

    Add files via upload

--- Home/Home.js
+++ Home/Home.js
@@ -1,30 +1,28 @@
+//Scrolling
 $(window).ready(function() {
+  $(".fade").fadeTo(500,0);
     $(window).scroll(function() {
       var windowBottom = $(this).scrollTop() + $(this).innerHeight();
       $(".fade").each(function() {
         /* Check the location of each desired element */
-        var objectBottom = $(this).offset().top + $(this).outerHeight();
+        var objectBottom = $(this).offset().top + $(this).outerHeight() -50;
         
         /* If the element is completely within bounds of the window, fade it in */
         if (objectBottom < windowBottom) 
         { //object comes into view (scrolling down)
           if ($(this).css("opacity")==0) {$(this).fadeTo(500,1);}
         } 
-        else 
-        { //object goes out of view (scrolling up)
-          if ($(this).css("opacity")==1) {$(this).fadeTo(500,0);}
-        }
       });
     }).scroll(); //invoke scroll-handler on page-load
   });
+    //Reset the scroll after refreshing the page
+    history.scrollRestoration = "manual";
+
+    $(window).on('beforeunload', function(){
+          $(window).scrollTop(0);
+  
+    });
+  //Pre-loader
   $(window).on("load",function(){
     $(".absCenter").fadeOut("slow");
-      $(body, html).css('overflow','hidden');
   });
-  $(window).load(function(){
-    $(body, html).addClass('hideMyScroll');
-    });
-    
-    $(document).ready(function(){
-    $(body, html).removeClass('hideMyScroll');
-    });
---
+++ Home/Home.php
@@ -0,0 +1,150 @@
+<!DOCTYPE html>
+<head>
+  <title>Capsule Pharmacy</title>
+  <!-- To Support Arabic Language in website-->
+  <meta charset="utf-8"/>
+  <!--Our website description for google search-->
+  <meta name="description"content="Order your medication from Capsule Pharmacy 24 hours a day, 7 days a week. Pay online or in cash at the time of delivery, use your medical insurance, and we will deliver your medication right to your door!"/>
+
+  <link rel="icon" type="image/x-icon" href="../images/icon.ico">
+
+  <!--Our CSS files/URLs-->
+  <link rel="stylesheet" href="Home.css?version=1">
+
+  <!--Our JS files/URLs-->
+  <script src="https://kit.fontawesome.com/f98208c8c8.js" crossorigin="anonymous"></script><!-- Ready made functions for icons ex:Location/Email or phone-->
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.1/jquery.min.js"></script>
+  <script src="Home.js?version=2"></script>
+</head>
+
+<body>
+  <!-- BASICS for the website <NAVBAR>(HOME)-->
+   <div class="navbar">
+      <nav>
+        <h1>Capsule<br><br> Pharmacy</h1>
+ <ul>
+    <li>
+        <a href="Home.php">Home</a>
+    </li>
+    <li>
+      <a href="..\Shop\Shop.php">Shop Now</a>
+  </li>
+    <li>
+      <a href="..\Contact us\Contact us.php">Contact Us</a>
+    </li>
+    <li>
+      <a href="..\About us\About us.php">About us</a>
+    </li>
+    <li>
+      <a href="..\Sign in\Sign in.php">Sign in</a>
+    </li>
+    <li>
+      <a href="..\Sign up\Sign up.php">Sign up</a>
+    </li>
+ </ul>
+</nav>
+  <!-- THE END <NAVBAR> -->
+<a href="#wrapper"> 
+  <button id="ourservices">Our Services</button> 
+</a>
+
+</div id="logoheader">
+<img id="bgdoctors" src="..\Images\Home\logodoc.png">
+<h1 class="logoword"></h1>
+</div>
+<div id="wrapper">
+  <div class="fade">
+<h5 id="BPService">Our Services</h5>
+<h1 id="BPHead">Blood Pressure</h1>
+<p id="BPPara">Blood pressure (BP) is the pressure of circulating blood against the walls of blood vessels.<br> Most of this pressure results from the heart pumping blood through the circulatory system.<br> When used without qualification, the term "blood pressure" refers to the pressure in the large arteries.</p>
+<img id="BPimg" src="..\Images\Home\BP.jpg">
+<a href="#wrapper2"> 
+  <button id="moreinfo1">More Info</button> 
+</a>
+</div>
+</div>
+<div id="wrapper2">
+  <div class="fade">
+  <h5 id="BloodGucservice">Our Services</h5>
+  <h1 id="gluchead"> Blood Glucose</h1>
+  <p id="glucpara">The blood glucose level is the amount of glucose in the blood.<br> Glucose is a sugar that comes from the foods we eat, and it's also formed and stored inside the body.<br> It's the main source of energy for the cells of our body, and it's carried to each cell through the bloodstream.</p>
+  <img id="BloodGlucimg" src="..\Images\Home\BloodGlu.jpg">
+  <a href="#wrapper3"> 
+    <button id="moreinfo2">More Info</button> 
+  </a>
+  </div>
+</div>
+
+
+<div id="wrapper3">
+  <div class="fade">
+  <h5 id="Weightservice">Our Services</h5>
+  <h1 id="Weighthead">Weight Measurement</h1>
+  <p id="weightpara">There is a weight and height measuring device in all our pharmacies<br> with some analyzes of your body that will help you understand your needs</p>
+  <img id="weightimg" src="..\Images\Home\WeightScale.jpg">
+  <a href="#wrapper4"> 
+    <button id="moreinfo3">More Info</button> 
+  </a>
+</div>
+</div>
+<div id="wrapper4">
+  <div class="fade">
+  <h5 id="deliveryservice">Our Services</h5>
+  <h1 id="deliveryhead">Home Delivery</h1>
+  <p id="deliverypara">We deliver our products to your location,<br> just call us at 1111 to order your product, or order online</p>
+  <img id="deliveryimg" src="..\Images\Home\Homedelivery.jpg">
+  <a href="..\Shop\Shop.php"> 
+    <button id="shopnowbutton"> Shop Now</button> 
+  </a>
+  </div>
+</div>
+<!--Pre-loader--> 
+<div class="absCenter ">
+  <div class="loaderPill">
+      <div class="loaderPill-anim">
+          <div class="loaderPill-anim-bounce">
+              <div class="loaderPill-anim-flop">
+                  <div class="loaderPill-pill"></div>
+              </div>
+          </div>
+      </div>
+      <div class="loaderPill-floor">
+          <div class="loaderPill-floor-shadow"></div>
+      </div>
+      <div class="loaderPill-text">Loading...Please wait</div>
+  </div>
+</div>
+</body>
+<!-- Website footer for some informations and references-->
+<footer>
+  <div id="footer">
+    <h4>Useful links</h4>
+    <ul>
+      <li>
+        <a href="..\Shop\Shop.php">Shop</a>
+    </li>
+    <li>
+      <a href="..\About us\About us.php">About us</a>
+    </li>
+      <li>
+        <a href="..\Contact us\Contact us.php">Contact Us</a>
+      </li>
+      <li>
+        <a href="..\Privacy policy\Privacy policy.php">Privacy policy</a>
+      </li>
+      <li>
+        <a href="..\Terms and Service\Terms and Service.php">Terms and Service</a>
+      </li>
+   </ul>
+   <h3>CONTACT INFORMATION</h3>
+   <p>Coming soon</p>
+   <p>Coming soon</p>
+   <p>Coming soon@comingsoon.com</p>
+   <i class="fa-solid fa-location-dot"style="font-size:20px; position: relative; left:300px; bottom:337px;"></i>
+   <i class="fa-solid fa-phone" style="font-size:20px; position: relative; left:280px; bottom:297px;"></i>
+   <i class="fa-solid fa-envelope" style="font-size:20px; position: relative; left:257px; bottom:252px;"></i>
+    </div>
+    <div id="footermap">
+      <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3456.7906226700115!2d30.95575751518746!3d29.95670058191425!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x1458569393ca36ab%3A0xc1f1038e9a84b7a4!2sMSA!5e0!3m2!1sen!2seg!4v1669477559297!5m2!1sen!2seg" style="border:0;" width="600" height="450" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+      </div>
+</footer>
